1afde93c0e7dfce39c867597aab1318685a9d9bd
felxx <igorfelipemonteirocontato@gmail.com>
2025-05-27 14:03:48 -0300

    implement basic chess game structure and a board display

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -1,37 +1,17 @@
 package chess;
 
-import boardgame.Position;
+import boardgame.Piece;
+import boardgame.Board;
 
-public abstract class ChessPiece {
+public class ChessPiece extends Piece {
 	private Color color;
-	private int moveCount;
 	
-	public ChessPiece(Color color, int moveCount) {
+	public ChessPiece(Board board, Color color) {
+		super(board);
 		this.color = color;
-		this.moveCount = moveCount;
 	}
 
 	public Color getColor() {
 		return color;
-	}
-
-	public void setColor(Color color) {
-		this.color = color;
-	}
-
-	public int getMoveCount() {
-		return moveCount;
-	}
-	
-	protected boolean isThereOpponentPiece(Position position) {
-		return false;
-	}
-	
-	protected void increaseMoveCount() {
-		
-	}
-	
-	protected void decreaseMoveCount() {
-		
 	}
 }
