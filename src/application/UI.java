8a6b13c98ff2dd4804318fa7588f486f4504e3c2
felxx <igorfelipemonteirocontato@gmail.com>
2025-05-29 14:10:33 -0300

    implement checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import chess.ChessMatch;
 import chess.ChessPiece;
@@ -54,10 +53,17 @@ public class UI {
         printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turn: "+chessMatch.getTurn());
-        System.out.println("Waiting player: "+chessMatch.getCurrentPlayer());
-        if(chessMatch.getCheck()){
+        if(!chessMatch.getCheckMate()){
+            System.out.println("Waiting player: "+chessMatch.getCurrentPlayer());
+            if(chessMatch.getCheck()){
+                System.out.println();
+                System.out.println("CHECK!");
+            }  
+        }
+        else{
             System.out.println();
-            System.out.println("CHECK!");
+            System.out.println("CHECKMATE!");
+            System.out.println("Winner: "+chessMatch.getCurrentPlayer());
         }
     }
 
